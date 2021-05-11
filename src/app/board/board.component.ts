9dbc4fcac23d7490d6d5bdbc3c06bd889c6a97f3
DrackPyros <santiagojovaniss@gmail.com>
2021-05-11 10:19:26 +0200

    crear frames

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { $ } from 'protractor';
+declare var $: any;
+
 
 export interface caja{
   url: string,
@@ -22,15 +23,87 @@ export class BoardComponent{
 
   onSubmit(){
     console.log(this.frame.url);
-
-    // $(document).ready(function(){
-    //   $()
-    // })
-
-    // var id = $("#test").attr("id");
-    // console.log(id);
+    // console.log($(this).attr('id'));
     
-    // var a = document.getElementById(id)
+    // var a = document.getElementById(x)
     // alert(event.target.id);
+    
+    if(!this.frame.url){
+      alert("Introduce una enlace de tipo <iframe>");
+    }
+    else
+      this.validate(this.frame.url);
+  }
+
+  // ---------------------------------------------
+  // f = "<iframe width='560' height='315' src='https://www.youtube.com/embed/JdeJ-ct6S4E' title='YouTube video player' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe>"
+  // console.log("a = "+frase);
+  // crearframe(f);
+  
+  validate(f: string){
+    // var frase = document.forms["pipo"]["pep"].value;
+    var frase = f;
+    console.log();
+
+    frase = frase.replace(/\"/g, "'");
+    // console.log("b = "+frase);
+    // console.log(typeof(frase2));    
+    this.crearframe(frase);
+  }
+
+  crearframe(frase: string){
+
+    // if(f == frase2){
+    //     console.log("iguales");
+    // }
+    
+    var frase2 = frase.split(" ");
+    var a = "-1";
+
+    for (let i in frase2){
+      if (frase2[i].includes("src")){
+        a = i;
+      }
+    }
+    var url = frase2[a];
+    url = url.slice(5, -1);
+    // console.log(url);
+    // console.log(typeof(url));
+
+    for (let i in frase2){
+      if (frase2[i].includes("width")){
+          a = i;
+      }
+    }
+    var wi = frase2[a];
+    wi = wi.slice(7, -1);
+    wi = wi+"px";
+    // console.log(wi);
+    // console.log(typeof(wi));
+
+    for (let i in frase2){
+      if (frase2[i].includes("height")){
+          a = i;
+      }
+    }
+    var he = frase2[a];
+    he = he.slice(8, -1);
+    he = he+"px";
+    // console.log(he);
+    // console.log(typeof(he));
+
+    // ----------------------------------------------
+    var frame = document.createElement("iframe");
+    frame.style.width = wi;
+    frame.style.height = he;
+    // frame.style.border = 0;
+    frame.setAttribute("src", url);
+
+    console.log(frame);
+
+    document.body.appendChild(frame);
+
+    // Youtube
+    // frame.setAttribute('allowFullScreen', '')
   }
 }
--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,5 +1,6 @@
 import { NgClass } from '@angular/common';
 import { Component, } from '@angular/core';
+declare var $: any;
 
 @Component({
   selector: 'app-menu',
@@ -8,13 +9,35 @@ import { Component, } from '@angular/core';
 })
 export class MenuComponent{
 
-  constructor() { }
+  public a: number;
+
+  constructor() {
+    this.a = 0;
+   }
 
   Creacaja(){
+    // Lienzo
     var back = document.getElementById("background");
+    
+    // Crear caja
     var box = document.createElement('div');
+    var b = document.createElement("button");
+
+    // Propiedades caja
     box.className = "draggable";
+    box.id = this.a.toString();
+
+    // Propiedades del boton
+    b.innerText = "Crealo";
+    b.addEventListener("click", () => {
+      alert($(this).attr('id'));
+    });
+
+    // Appends
+    box.appendChild(b);
     back.appendChild(box);
+
+    this.a += 1;
   }
 
 }
