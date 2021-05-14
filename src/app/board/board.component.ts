91b17383741c0857c2ed4462a565217566a34d98
DrackPyros <72030593+DrackPyros@users.noreply.github.com>
2021-05-14 11:59:21 +0200

    cajas funcionan

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -3,98 +3,92 @@ declare var $: any;
 
 
 export interface caja{
-  url: string,
-  width: string,
-  height: string
+    url: string,
 }
 
 @Component({
-  selector: 'app-board',
-  templateUrl: './board.component.html',
-  styleUrls: ['./board.component.scss']
+    selector: 'app-board',
+    templateUrl: './board.component.html',
+    styleUrls: ['./board.component.scss']
 })
 export class BoardComponent{
 
-  public frame: caja;
+    public frame: caja;
 
-  constructor() {
-    this.frame= { url:"", width:"", height:"" }
-   }
+    constructor() {
+        this.frame= { url:""}
+    }
 
-  onSubmit(i : number){
-    console.log(this.frame.url);
-    // console.log($(this).attr('id'));
+    onSubmit(i : number){
+        console.log(this.frame.url);
+        // console.log($(this).attr('id'));
+        
+        // var a = document.getElementById(x)
+        // alert(event.target.id);
+        // console.log(this);
+        if(!this.frame.url){
+            alert("Introduce una enlace de tipo <iframe>");
+        }
+        else
+            this.validate(this.frame.url);
+    }
     
-    // var a = document.getElementById(x)
-    // alert(event.target.id);
-    
-    if(!this.frame.url){
-      alert("Introduce una enlace de tipo <iframe>");
+    validate(f: string){
+        var frase = f;
+        console.log();
+        frase = frase.replace(/\"/g, "'");  
+        this.crearframe(frase);
     }
-    else
-      this.validate(this.frame.url);
-  }
 
-  // ---------------------------------------------
-  // f = "<iframe width='560' height='315' src='https://www.youtube.com/embed/JdeJ-ct6S4E' title='YouTube video player' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe>"
-  // crearframe(f);
-  
-  validate(f: string){
-    var frase = f;
-    console.log();
-    frase = frase.replace(/\"/g, "'");  
-    this.crearframe(frase);
-  }
+    crearframe(frase: string){
+        var frase2 = frase.split(" ");
+        var a = "-1";
 
-  crearframe(frase: string){
-    var frase2 = frase.split(" ");
-    var a = "-1";
+        for (let i in frase2){
+            if (frase2[i].includes("src")){
+                a = i;
+            }
+        }
+        var url = frase2[a];
+        url = url.slice(5, -1);
+        // console.log(url);
+        // console.log(typeof(url));
 
-    for (let i in frase2){
-      if (frase2[i].includes("src")){
-        a = i;
-      }
+        for (let i in frase2){
+            if (frase2[i].includes("width")){
+                a = i;
+            }
+        }
+        var wi = frase2[a];
+        wi = wi.slice(7, -1);
+        wi = wi + "px";
+        // console.log(wi);
+        // console.log(typeof(wi));
+
+        for (let i in frase2){
+            if (frase2[i].includes("height")){
+                a = i;
+            }
+        }
+        var he = frase2[a];
+        he = he.slice(8, -1);
+        he = he + "px";
+        // console.log(he);
+        // console.log(typeof(he));
+
+        // ----------------------------------------------
+        var back = document.getElementsByClassName("content"); // Coger id para crear iframe en la caja
+
+        var frame = document.createElement("iframe");
+        frame.style.width = wi;
+        frame.style.height = he;
+        frame.setAttribute("src", url);
+
+        console.log(frame);
+
+        document.body.appendChild(frame);
+
+        // Youtube
+        // frame.setAttribute('allowFullScreen', '')
     }
-    var url = frase2[a];
-    url = url.slice(5, -1);
-    // console.log(url);
-    // console.log(typeof(url));
-
-    for (let i in frase2){
-      if (frase2[i].includes("width")){
-          a = i;
-      }
-    }
-    var wi = frase2[a];
-    wi = wi.slice(7, -1);
-    wi = wi+"px";
-    // console.log(wi);
-    // console.log(typeof(wi));
-
-    for (let i in frase2){
-      if (frase2[i].includes("height")){
-          a = i;
-      }
-    }
-    var he = frase2[a];
-    he = he.slice(8, -1);
-    he = he+"px";
-    // console.log(he);
-    // console.log(typeof(he));
-
-    // ----------------------------------------------
-    var back = document.getElementsByClassName("content"); // Coger id para crear iframe en la caja
-
-    var frame = document.createElement("iframe");
-    frame.style.width = wi;
-    frame.style.height = he;
-    frame.setAttribute("src", url);
-
-    console.log(frame);
-
-    document.body.appendChild(frame);
-
-    // Youtube
-    // frame.setAttribute('allowFullScreen', '')
-  }
 }
--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,116 +1,149 @@
-import { NgClass } from '@angular/common';
-import { Component, } from '@angular/core';
+import { Component, OnChanges, OnInit} from '@angular/core';
 declare var $: any;
 
 @Component({
-  selector: 'app-menu',
-  templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.scss']
+    selector: 'app-menu',
+    templateUrl: './menu.component.html',
+    styleUrls: ['./menu.component.scss']
 })
-export class MenuComponent{
+export class MenuComponent implements OnInit{
 
-  public a: number;
+    public a: number;
 
-  constructor() {
-    this.a = 0;
-   }
-
-  Creacaja(i: number){
-    // Lienzo
-    var back = document.getElementById("background");
-    
-    if( i == 4){ // If Twitch Stream
-      // Crear caja
-      var box = document.createElement('div');
-
-      // Propiedades caja
-      box.className = "draggable";
-      box.id = this.a.toString();
-
-      // Crear form
-      var lab1 = document.createElement("label");
-      lab1.setAttribute("for", "channel");
-      lab1.innerHTML = "Nombre del Streamer";
-      
-      var channel = document.createElement("input");
-      channel.setAttribute("type", "text");
-      channel.setAttribute("name", "channel");
-
-      var r1 = document.createElement("input");
-      r1.setAttribute("type", "radio");
-      r1.setAttribute("name", "chat");
-      r1.setAttribute("value", "yes");
-
-      var lab2 = document.createElement("label");
-      lab2.setAttribute("for", "r1");
-      lab2.innerHTML = "Con chat";
-      
-      var r2 = document.createElement("input");
-      r2.setAttribute("type", "radio");
-      r2.setAttribute("name", "chat");
-      r2.setAttribute("value", "no");
-
-      var lab3 = document.createElement("label");
-      lab3.setAttribute("for", "r2");
-      lab3.innerHTML = "Sin chat";
-
-      // Boton
-      var b = document.createElement("button");
-      b.innerText = "Crealo";
-
-      // b.addEventListener("click", onSubmit(i));
-
-      // b.addEventListener("click", () => {
-        // b.onclick = onSubmit(i); // especificar la funcion onSubmit de boardComponent
-        // alert($(this).attr('id'));
-      // });
-
-      // Appends
-      box.appendChild(lab1);
-      box.appendChild(document.createElement("br"));
-      box.appendChild(channel);
-      box.appendChild(document.createElement("br"));
-      box.appendChild(b);
-      box.appendChild(document.createElement("br"));
-      box.appendChild(r1);
-      box.appendChild(lab2);
-      box.appendChild(document.createElement("br"));
-      box.appendChild(r2);
-      box.appendChild(lab3);
+    constructor() {
+        this.a = 0;
     }
-    else{
-      // Crear caja
-      var box = document.createElement('div');
-      
-      // Propiedades caja
-      box.className = "draggable";
-      box.id = this.a.toString();
-
-      // Titulo
-      var t = document.createElement("h2");
-      t.innerHTML = "Pega el código del &#60;iframe&#62; aquí";
-      
-      // Crear Form
-      var f = document.createElement("input");
-      f.setAttribute("type", "text");
-      // f.setAttribute("#caja", "ngModel");
-      // f.setAttribute("[(ngModel)]", "frame.url");
-      
-      // Propiedades del boton
-      var b = document.createElement("button");
-      b.innerText = "Crealo";
-      b.addEventListener("click", () => {
-        // b.onclick = onSubmit(i); // especificar la funcion onSubmit de boardComponent
-        alert($(this).attr('id'));
-      });
-
-      // Appends
-      box.appendChild(t);
-      box.appendChild(f);
-      box.appendChild(document.createElement("br"));
-      box.appendChild(b);
+    ngOnInit(): void{
+        this.Creacaja(5);
     }
-    back.appendChild(box);
-    this.a += 1;
-  }
+
+    prop(): void { // Añadir propiedades a las cajas
+        console.log("Pipo es un buen perro");
+        $( function inicio() {
+            $( ".draggable" )
+                .draggable(
+                    {containment: "#background"}, // especifica respecto a qué es dragable
+                        {start: function(e: any, ui: any) {
+                            // console.log('me drago mucho');
+                        }},
+                    ) 
+        
+                .resizable(
+                    {containment: "#background"}, // especifica respecto a qué es resizable
+                        {start: function(e: any, ui: any) {
+                        // console.log('me resizean vivamente');
+                        }} 
+                    );
+                } 
+        );
+    }
+    Creacaja(i: number){
+        // Lienzo
+        var back = document.getElementById("background");
+        
+        if( i == 4){ // If Twitch Stream
+            // Crear caja
+            var box = document.createElement('div');
+
+            // Propiedades caja
+            box.className = "draggable";
+            box.id = this.a.toString();
+
+            // Crear form
+            var lab1 = document.createElement("label");
+            lab1.setAttribute("for", "channel");
+            lab1.innerHTML = "Nombre del Streamer";
+            
+            var channel = document.createElement("input");
+            channel.setAttribute("type", "text");
+            channel.setAttribute("name", "channel");
+
+            var r1 = document.createElement("input");
+            r1.setAttribute("type", "radio");
+            r1.setAttribute("name", "chat");
+            r1.setAttribute("value", "yes");
+
+            var lab2 = document.createElement("label");
+            lab2.setAttribute("for", "r1");
+            lab2.innerHTML = "Con chat";
+            
+            var r2 = document.createElement("input");
+            r2.setAttribute("type", "radio");
+            r2.setAttribute("name", "chat");
+            r2.setAttribute("value", "no");
+
+            var lab3 = document.createElement("label");
+            lab3.setAttribute("for", "r2");
+            lab3.innerHTML = "Sin chat";
+
+            // Boton
+            var b = document.createElement("button");
+            b.innerText = "Crealo";
+
+            // b.addEventListener("click", onSubmit(i));
+
+            // b.addEventListener("click", () => {
+                // b.onclick = onSubmit(i); // especificar la funcion onSubmit de boardComponent
+                // alert($(this).attr('id'));
+            // });
+
+            // Appends
+            box.appendChild(lab1);
+            box.appendChild(document.createElement("br"));
+            box.appendChild(channel);
+            box.appendChild(document.createElement("br"));
+            box.appendChild(b);
+            box.appendChild(document.createElement("br"));
+            box.appendChild(r1);
+            box.appendChild(lab2);
+            box.appendChild(document.createElement("br"));
+            box.appendChild(r2);
+            box.appendChild(lab3);
+        }
+        else{
+            // Crear caja
+            var box = document.createElement('div');
+            
+            // Propiedades caja
+            box.classList.add("draggable");
+            box.id = this.a.toString();
+
+            // Contenedor general
+            var content = document.createElement('div');
+            content.className = "content";
+
+            // Titulo
+            var t = document.createElement("h2");
+            t.innerHTML = "Pega el código del &#60;iframe&#62; aquí";
+            
+            // Crear Form
+            var c = document.createElement('div');
+            c.className = "mx-auto";
+
+            var f = document.createElement("input");
+            f.setAttribute("type", "text");
+            f.setAttribute("#caja", "ngModel");
+            f.setAttribute("[(ngModel)]", "frame.url");
+            
+            // Propiedades del boton
+            var b = document.createElement("button");
+            b.className = "btn btn-primary";
+            b.innerText = "Crealo";
+            b.addEventListener("click", () => {
+                // b.onclick = onSubmit(i); // especificar la funcion onSubmit de boardComponent
+                alert($(this).attr('id'));
+            });
+
+            // Appends
+            content.appendChild(t);
+            c.appendChild(f);
+            c.appendChild(document.createElement("br"));
+            c.appendChild(b);
+            content.appendChild(c);
+            box.appendChild(content);
+        }
+        back.appendChild(box);
+        this.a += 1;
+        this.prop();
+    }
 }
